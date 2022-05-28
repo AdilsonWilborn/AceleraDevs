950f388405b3dcddc59abb7c542174378327fc8d
Adilson Wilborn Santana <84751676+AdilsonWilborn@users.noreply.github.com>
2022-05-28 16:50:10 -0300

    Add files via upload

---
+++ Aula02/Aula01.java
@@ -0,0 +1,8 @@
+package com.stefanini.aprendendocomeclipse.aula;
+
+public class Aula01 {
+	public static void main (String[] args) {
+		Aula03 aula = new Aula03();
+		System.out.println(aula);
+	}
+}
---
+++ Aula02/Aula03.java
@@ -0,0 +1,9 @@
+package com.stefanini.aprendendocomeclipse.aula;
+
+public class Aula03 {
+	public static void main (String[] args) {
+		
+		System.out.println("Aqui é a raiz");
+		
+	}
+}
